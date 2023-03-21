864bc6840d1321a357098fa93f01a7f342bfbe82
Andrew Littlejohn <andrewlittlejohn@andrews-air.myfiosgateway.com>
2023-03-21 10:33:01 -0400

    March 21

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -68,29 +68,79 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
-  // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
+// router.put('/:id', (req, res) => {
+//   // update product data
+//   Product.update(
+//     { 
+//       product_name: req.body.product_name,
+//       price: req.body.price,
+//       stock: req.body.stock,
+//       category_id: req.body.category_id
+//     },  
+//     {
+//       where: {
+//         id: req.params.id,
+//       },
+//     }
+//   )
+//     .then((product) => {
+//       // find all associated tags from ProductTag
+//       return ProductTag.findAll({ where: { product_id: req.params.id } });
+//     })
+//     .then((productTags) => {
+//       // get list of current tag_ids
+//       const productTagIds = productTags.map(({ tag_id }) => tag_id);
+//       // create filtered list of new tag_ids
+//       const newProductTags = req.body.tagIds
+//         .filter((tag_id) => !productTagIds.includes(tag_id))
+//         .map((tag_id) => {
+//           return {
+//             product_id: req.params.id,
+//             tag_id,
+//           };
+//         });
+//       // figure out which ones to remove
+//       const productTagsToRemove = productTags
+//         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+//         .map(({ id }) => id);
+
+//       // run both actions
+//       return Promise.all([
+//         ProductTag.destroy({ where: { id: productTagsToRemove } }),
+//         ProductTag.bulkCreate(newProductTags),
+//       ]);
+//     })
+//     .then((updatedProductTags) => res.json(updatedProductTags))
+//     .catch((err) => {
+//       // console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { product_name, price, stock, category_id, tagIds } = req.body;
+
+    // Update product
+    await Product.update(
+      { product_name, price, stock, category_id },
+      { where: { id } }
+    );
+
+    // Find all associated tags
+    const productTags = await ProductTag.findAll({ where: { product_id: id } });
+
+    // Get current tag IDs
+    const currentTagIds = productTags.map(({ tag_id }) => tag_id);
+
+    // Create filtered list of new tag IDs
+    const newTagIds = tagIds.filter(tagId => !currentTagIds.includes(tagId));
+
+    // Create array of new ProductTag objects to bulk create
+    const newProductTags = newTagIds.map(tagId => {
+      return { product_id: id, tag_id: tagId };
+    });
       // figure out which ones to remove
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
